5e49516404dd1f40f691525941477c5960548fe7
Fábio Matias <zeosamaster@users.noreply.github.com>
2022-04-18 21:54:58 +0100

    Whitelist dApp (#1)
    * feat(contract): add contract
    
    * feat(ui): add UI for contract

---
+++ whitelist-dapp/hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const { ALCHEMY_API_KEY_URL, RINKEBY_PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY]
+    }
+  }
+};
